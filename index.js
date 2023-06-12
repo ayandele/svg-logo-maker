8736bfbdff6f5d25b391ae50246f50a5826ff2f9
ayandele <adele.ayansola@yahoo.com>
2023-06-12 14:05:40 -0400

    code revision and path update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { Triangle, Circle, Square } = require('./lib/shapes');
+const { Triangle, Circle, Square } = require('./lib/shapes.js');
 
 const promptUser = () => {
 return inquirer.prompt([
